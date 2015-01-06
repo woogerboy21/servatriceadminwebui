f5c6fe019ed7e282407dd6417b5307ae5711419c
Zach H <Zoosorbs@gmail.com>
2015-01-05 19:06:56 -0500

    Log files & Redirect

--- html/loginpage.php
+++ html/loginpage.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-<title>Cockatrice Account Management</title>
+	<title>Cockatrice Account Management</title>
 </head>
 <html>
 	<body>
@@ -15,7 +15,7 @@
 			<table align="center" border="1" cellpadding="5">
 				<tr><td colspan="2" align="center"><a href="http://www.woogerworks.com">Home</a></td></tr>
 				<tr><td>Username:</td><td><input type="text" name="inputeduname" maxlength="35" value="" size="35"/></td></tr>
-				<tr><td>Password:</td><td><input type="password" name="inputedpword" maxlength="120" value="" size="35"/></td></tr>
+				<tr><td>Password:</td><td><input type="password" name="inputedpword" maxlength="120" value="" size="35"/><input type="hidden" name="redirect" value="<?=$_GET['redirect']?>"></td></tr>
 				<tr><td colspan="2" align="center"><input type="submit" value="Log-in" /></td></tr>
 			</table>
 		</form>
--- html/viewlog.php
+++ html/viewlog.php
@@ -11,9 +11,7 @@
 <body>
 	<?php
 		require '.auth_modsession';
-		require '.config';
 		require '.config_commonfunctions';
-		global $configfile;
 		?>
 	<center>
 	<form method="POST">
@@ -72,148 +70,10 @@
 	</form>
 	<br/><br/>
 <?php
-if (count($_REQUEST) > 0)
-{	
-	if (strlen($_REQUEST['username']) > 0) // Check if a username is given
+	if (count($_REQUEST) > 0)
 	{
-		$username = strtolower(mysql_real_escape_string($_REQUEST['username']));
+		build_log_table();
 	}
-
-	if (strlen($_REQUEST['ip_address']) > 0) // Check if IP is given
-	{
-		$ip_to_find = mysql_real_escape_string($_REQUEST['ip_address']);
-	}
-	
-	if (intval($_REQUEST['game_id'] > 0)) // Check if Game ID is given
-	{
-		$game_id = intval(mysql_real_escape_string($_REQUEST['game_id']));
-	}
-	
-	if (strlen($_REQUEST['game_name']) > 0) // Check if Game Name is given
-	{
-		$game_name = mysql_real_escape_string(strtolower($_REQUEST['game_name']));
-	}
-	
-	if ($_REQUEST['from_date']) // Check if date range is given
-	{
-		$total_time = mysql_real_escape_string($_REQUEST['from_date']);
-	}
-	
-	if (count($_REQUEST['type_of_chat']) > 0) // Check if any specific locations are given
-	{
-		$get_logs_from = array();
-		for ($i = 0; $i < 3; $i++)
-		{
-			if (isset($_REQUEST['type_of_chat'][$i]))
-			{
-				$get_logs_from[] = mysql_real_escape_string($_REQUEST['type_of_chat'][$i]);
-			}
-		}
-	}
-	
-	/* BUILD QUERY BASE */
-	$max_results_q = (intval($_REQUEST['max_results']) > 0) ? "LIMIT 0," . intval($_REQUEST['max_results']) : "";
-	$user_q = (isset($username)) ? "AND (`sender_name` = '$username' OR `target_name` = '$username')" : "";
-	$ip_q = isset($ip_to_find) ? "AND `sender_ip` = '$ip_to_find'" : "";
-	$game_id_q = isset($game_id) ? "AND (`target_id` = '$game_id' AND `target_type` = 'game')" : "";
-	$game_name_q = isset($game_name) ? "AND (`target_name` = '$game_name' AND `target_type` = 'game')" : "";
-
-	switch ($total_time)
-	{	
-		case "week":
-			$date_q = "AND `log_time` >= DATE_SUB(NOW(), INTERVAL 1 WEEK)";
-		break;
-			
-		case "day":
-			$date_q = "AND `log_time` >= DATE_SUB(NOW(), INTERVAL 1 DAY)";
-		break;
-		
-		case "hour":
-			$date_q = "AND `log_time` >= DATE_SUB(NOW(), INTERVAL 1 HOUR)";
-		break;
-		
-		default:
-			$date_q = "";
-		break;
-	}
-
-	// Print out the tables, but in the format: Room, Game, Chat (3 different tables) based on what's requested
-	if (count($get_logs_from) == 0 && !isset($game_id) && !isset($game_name)) // Default: Show all 3 Tables
-	{
-		$get_logs_from = array("room", "game", "chat");
-	}
-	else if (isset($game_id) || isset($game_name)) // If getting a game room log, must change it to just show game room
-	{
-		$get_logs_from = array("game");
-	}
-
-	for ($i = 0; $i < count($get_logs_from); $i++)
-	{
-		$target_type_q = (strlen($game_id_q) == 0) ? "AND `target_type` = '{$get_logs_from[$i]}'" : ""; // Builds target query and avoids issues
-		$query = mysql_query("SELECT * FROM `cockatrice_log` WHERE `sender_ip` IS NOT NULL $user_q $date_q $ip_q $game_id_q $target_type_q $game_name_q ORDER BY -log_time $max_results_q") or die(mysql_error());
-		
-		switch ($get_logs_from[$i])
-		{
-			case "room":
-				echo "<table border=1><tr><th colspan=4>Main Chat Room Logs</th></tr>\n";
-				echo "<tr><th width=150px>Sender</th><th width=120px>IP Address</th><th width=300px>Message</th><th width=150px>Time Stamp</th></tr>\n";
-				while ($row = mysql_fetch_array($query))
-				{
-					$username = $row['sender_name'];
-					$name_with_link = "<a href='?username=$username'>$username</a>";
-					
-					$ip = $row['sender_ip'];
-					$ip_with_link = "<a href='?ip_address=$ip'>$ip</a>";
-					
-					echo "<tr><td>$name_with_link</td><td>$ip_with_link</td><td>{$row['log_message']}</td><td>{$row['log_time']}</td></tr>\n";
-				}
-				echo "</table><br/>\n";
-			break;
-			
-			case "game":
-				echo "<table border=1><tr><th colspan=6>Game Chat Logs</th></tr>\n";
-				echo "<tr><th width=150px>Sender</th><th width=120px>IP Address</th><th width=300px>Message</th><th width=150px>Game ID</th>";
-				echo "<th width=200px>Game Name</th><th width=150px>Time Stamp</th></tr>\n";
-				while ($row = mysql_fetch_array($query))
-				{
-					$username = $row['sender_name'];
-					$name_with_link = "<a href='?username=$username'>$username</a>";
-					
-					$ip = $row['sender_ip'];
-					$ip_with_link = "<a href='?ip_address=$ip'>$ip</a>";
-					
-					$room_id = $row['target_id'];
-					$room_id_with_link = "<a href='?game_id=$room_id'>$room_id</a>";
-					
-					$game_name = $row['target_name'];
-					$game_name_with_link = "<a href='?game_name=$game_name'>$game_name</a>";
-					
-					echo "<tr><td>$name_with_link</td><td>$ip_with_link</td><td>{$row['log_message']}</td><td>$room_id_with_link</td><td>$game_name_with_link</td><td>{$row['log_time']}</td></tr>\n";
-				}
-				echo "</table><br/>\n";
-			break;
-			
-			case "chat":
-				echo "<table border=1><tr><th colspan=5>Private Chat Logs</th></tr>\n";
-				echo "<tr><th width=150px>Sender</th><th width=120px>IP Address</th><th width=300px>Message</th><th width=200px>Receiver</th><th width=150px>Time Stamp</th></tr>\n";
-				while ($row = mysql_fetch_array($query))
-				{
-					$username = $row['sender_name'];
-					$name_with_link = "<a href='?username=$username'>$username</a>";
-				
-					$ip = $row['sender_ip'];
-					$ip_with_link = "<a href='?ip_address=$ip'>$ip</a>";
-					
-					$receiver = $row['target_name'];
-					$receiver_with_link = "<a href='?username=$receiver'>$receiver</a>";
-				
-					echo "<tr><td>$name_with_link</td><td>$ip_with_link</td><td>{$row['log_message']}</td><td>$receiver_with_link</td><td>{$row['log_time']}</td></tr>\n";
-				}
-				echo "</table><br/>\n";
-			break;
-		}
-	}
-}
 ?>
 </center>
 </body>
