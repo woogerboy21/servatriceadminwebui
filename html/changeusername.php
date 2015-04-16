d48bf14899750545382265b5c23b5531965609bf
Zach H <Zoosorbs@gmail.com>
2015-04-16 16:32:18 -0400

    update change username
    file updates

--- html/changeusername.php
+++ html/changeusername.php
@@ -17,7 +17,7 @@
             <td><input type="text" name="cur" placeHolder="Current Username"></td>
         </tr>
         <tr>
-            <td><input type="text" name="new" maxlength="20" placeHolder="New Username"></td>
+            <td><input type="text" name="new" maxlength="30" placeHolder="New Username"></td>
         </tr>
         <tr>
             <td><input type="password" name="hash" placeHolder="Current Password"></td>
@@ -27,10 +27,27 @@
                 <input type="submit" value="Change Username" style="width:150px">
             </td>
         </tr>
+        <tr>
+            <td>
+                <div style="text-align:left;">
+                    <ul>
+                        <li>You may use any of the following characters in your name: A-Z, a-z, 0-9, _, -, and .</li>
+                        <li>You may only have up to 30 characters in your new username</li>
+                        <li>You may only change your username if your old username was invalid</li>
+                        <li>You may only change your username <u>once</u></li>
+                    </ul>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <font color="red"><?php if ($_POST) echo "<hr width='50%'>" . change_username($_POST['cur'], $_POST['new'], $_POST['hash']); ?></font>
+            </td>
+        </tr>
     </table>
     </form>
     <br/><br/>
-    <font color="red"><?php if ($_POST) echo change_username($_POST['cur'], $_POST['new'], $_POST['hash']); ?></font>
+    
     </center>
 </body>
 </html>
